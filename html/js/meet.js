0aba223af9bc472e2408a2301d354dfd523fe1f0
Belem Zhang <belem.zhang@intel.com>
2021-06-30 10:51:53 +0800

    camera utils hacked

--- html/js/meet.js
+++ html/js/meet.js
@@ -1,161 +1,242 @@
-const inputvideo_owt = $("#inputvideo_owt")[0]
-const inputvideo_mp = $("#inputvideo_mp")[0]
-let conference = new Owt.Conference.ConferenceClient()
-let room, myid
-let stream, processedstream
-let localStream
-let publicationGlobal
-let isSelf
-let resolution = { width: 1280, height: 720 }
+// const inputvideo_owt = $("#inputvideo_owt")[0]
+const inputvideo_mp = $("#inputvideo_mp")[0];
+const canvasCtx = $("#outputcanvas")[0].getContext("2d");
+let cW = $("#outputcanvas")[0].width;
+let cH = $("#outputcanvas")[0].height;
+let camera,
+  isbr = false;
+
+let conference = new Owt.Conference.ConferenceClient();
+let room, myid;
+let processedstream;
+let localStream;
+let publicationGlobal;
+let isSelf;
 let avTrackConstraint = {
   audio: {
     source: "mic",
   },
   video: {
-    resolution: new Owt.Base.Resolution(1280, 720),
+    resolution: resolution,
     frameRate: 24,
     source: "camera",
   },
-}
+};
 
-const createOWTStream = async () => {
-  stream = await Owt.Base.MediaStreamFactory.createMediaStream(
-    avTrackConstraint
-  )
-  if ("srcObject" in inputvideo_owt) {
-    inputvideo_owt.srcObject = stream
-  } else {
-    inputvideo_owt.src = URL.createObjectURL(stream)
+const initConference = () => {
+  resolution = { width: 320, height: 240 };
+  if ($("#login-480").hasClass("selected")) {
+    resolution = { width: 640, height: 480 };
+  } else if ($("#login-720").hasClass("selected")) {
+    resolution = { width: 1280, height: 720 };
   }
-
-  inputvideo_owt.autoplay = true
-}
-
-const screateMPStream = () => {
-  const camera = new Camera(inputvideo_mp, {
-    onFrame: async () => {
-      await selfieSegmentation.send({ image: inputvideo_mp })
-    },
-    width: 1280,
-    height: 720,
-  })
-
-  camera.start()
-
-//   stream = document.querySelector("#inputvideo_mp").srcObject
-
-//   console.log(stream)
-  
-  processedstream = document.querySelector("#outputcanvas").captureStream()
-  const audiotrack = stream.getAudioTracks()[0]
-  processedstream.addTrack(audiotrack)
-}
+};
 
 const subscribeVideo = (remotestream) => {
-  console.log("---- subscribeVideo ----")
-  console.log("---- remotestream.id ----")
-  console.log(remotestream.id)
+  console.log(remotestream.id);
 
   const subscribeOption = {
     audio: true,
     video: resolution,
-  }
+  };
 
   conference.subscribe(remotestream, subscribeOption).then(
     (subscription) => {
       let $video = $(
         `<video autoplay id=${remotestream.id} style="display:block" >this browser does not supported video tag</video>`
-      )
-      $video.get(0).srcObject = remotestream.mediaStream
-      $("#video-panel").append($video)
+      );
+      $video.get(0).srcObject = remotestream.mediaStream;
+      $("#video-panel").append($video);
     },
     (err) => {
-      console.log("subscribe failed", err)
+      console.log("subscribe failed", err);
     }
-  )
+  );
 
   remotestream.addEventListener("ended", () => {
-    $(`#${remotestream.id}`).remove()
-    $(`#${remotestream.id}resolutions`).remove()
-  })
+    $(`#${remotestream.id}`).remove();
+    $(`#${remotestream.id}resolutions`).remove();
+  });
 
   remotestream.addEventListener("updated", () => {
     //
-  })
-}
+  });
+};
 
 conference.addEventListener("streamadded", (event) => {
-  console.log("A new stream is added ", event.stream.id)
-  isSelf = isSelf ? isSelf : event.stream.id != publicationGlobal.id
+  console.log("A new stream is added ", event.stream.id);
+  isSelf = isSelf ? isSelf : event.stream.id != publicationGlobal.id;
 
-  console.log(isSelf)
-  console.log("---- event.stream.id ----")
-  console.log(event.stream.id)
+  console.log(isSelf);
+  console.log("---- event.stream.id ----");
+  console.log(event.stream.id);
 
-  console.log("---- publicationGlobal.id ----")
-  console.log(publicationGlobal.id)
+  console.log("---- publicationGlobal.id ----");
+  console.log(publicationGlobal.id);
 
-  isSelf && subscribeVideo(event.stream)
-  mixStream(room, event.stream.id, "common")
+  isSelf && subscribeVideo(event.stream);
+  mixStream(room, event.stream.id, "common");
   event.stream.addEventListener("ended", () => {
-    console.log(event.stream.id + " is ended.")
-  })
-})
+    console.log(event.stream.id + " is ended.");
+  });
+});
 
 const publishStream = () => {
   createToken(room, "user", "presenter", function (response) {
-    let token = response
+    let token = response;
     conference.join(token).then((resp) => {
       // myid = resp.self.id
-      room = resp.id
+      room = resp.id;
 
-      let streams = resp.remoteStreams
+      let streams = resp.remoteStreams;
       for (const stream of streams) {
         if (stream.source.audio !== "mixed") {
-          subscribeVideo(stream)
+          subscribeVideo(stream);
         }
       }
-      console.log("Streams in conference:", streams.length)
-      console.log("Participants in conference: " + resp.participants.length)
-      console.log("Participants: ")
-      console.log(resp.participants)
+      console.log("Streams in conference:", streams.length);
+      console.log("Participants in conference: " + resp.participants.length);
+      console.log("Participants: ");
+      console.log(resp.participants);
 
       localStream = new Owt.Base.LocalStream(
         processedstream,
         new Owt.Base.StreamSourceInfo("mic", "camera")
-      )
+      );
 
       conference.publish(localStream).then((publication) => {
-        publicationGlobal = publication
-        mixStream(room, publication.id, "common")
+        publicationGlobal = publication;
+        mixStream(room, publication.id, "common");
         publication.addEventListener("error", (err) => {
-          console.log("Publication error: " + err.error.message)
-        })
-      })
-    })
-  })
-}
+          console.log("Publication error: " + err.error.message);
+        });
+      });
+    });
+  });
+};
 
 const userExit = () => {
   if (localStream) {
     localStream.mediaStream.getTracks().forEach((track) => {
-      track.stop()
-    })
+      track.stop();
+    });
   }
-  localStream = undefined
-  conference.leave()
-  publicationGlobal.stop()
-  location.reload()
+  localStream = undefined;
+  conference.leave();
+  console.log("-- publicationGlobal --");
+  console.log(publicationGlobal);
+  publicationGlobal.stop();
+  location.reload();
+};
+
+// const createOWTStream = async () => {
+//   stream = await Owt.Base.MediaStreamFactory.createMediaStream(
+//     avTrackConstraint
+//   )
+//   if ("srcObject" in inputvideo_owt) {
+//     inputvideo_owt.srcObject = stream
+//   } else {
+//     inputvideo_owt.src = URL.createObjectURL(stream)
+//   }
+
+//   inputvideo_owt.autoplay = true
+// }
+
+const initStream = () => {
+  camera = new Camera(inputvideo_mp, {
+    onFrame: async () => {
+      await selfieSegmentation.send({ image: inputvideo_mp });
+    },
+    width: resolution.width,
+    height: resolution.height,
+  });
+};
+
+const getProcessedStream = () => {
+  processedstream = document.querySelector("#outputcanvas").captureStream();
+  const audiotrack = stream.getAudioTracks()[0];
+  processedstream.addTrack(audiotrack);
+};
+
+let activeEffect = "background";
+const fillColor = "rgba(204, 255, 144, 1.0)";
+const bg = document.querySelector("#bgdefault");
+const bgfilebutton = document.querySelector("#bgimg");
+const bgselector = document.querySelector(".bgselector");
+ 
+
+bgselector.addEventListener(
+  "click",
+  (e) => {
+    console.log(e.target);
+    console.log(e.target.img.src);
+  },
+  false
+)
+
+bgfilebutton.addEventListener(
+  "change",
+  (e) => {
+    const files = e.target.files;
+    if (files.length > 0) {
+      bg.src = URL.createObjectURL(files[0]);
+    }
+  },
+  false
+)
+
+function onResults(results) {
+  if (!isbr) {
+    canvasCtx.drawImage(results.image, 0, 0, cW, cH);
+  } else {
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, cW, cH);
+    canvasCtx.drawImage(results.segmentationMask, 0, 0, cW, cH);
+
+    // Only overwrite existing pixels.
+    // Only overwrite existing pixels.
+    canvasCtx.globalCompositeOperation = "source-in";
+    if (activeEffect === "mask" || activeEffect === "both") {
+      // This can be a color or a texture or whatever...
+      canvasCtx.fillStyle = fillColor;
+      canvasCtx.fillRect(0, 0, cW, cH);
+    } else {
+      canvasCtx.drawImage(results.image, 0, 0, cW, cH);
+    }
+
+    // Only overwrite missing pixels.
+    canvasCtx.globalCompositeOperation = "destination-atop";
+    if (activeEffect === "background" || activeEffect === "both") {
+      // This can be a color or a texture or whatever...
+      //   canvasCtx.fillStyle = fillColor
+      //   canvasCtx.fillRect(0, 0, cW, cH)
+      canvasCtx.drawImage(bg, 0, 0, cW, cH);
+    } else {
+      canvasCtx.drawImage(results.image, 0, 0, cW, cH);
+    }
+
+    canvasCtx.restore();
+  }
 }
+
+const selfieSegmentation = new SelfieSegmentation({
+  locateFile: (file) => {
+    console.log(file);
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
+  },
+});
+
+selfieSegmentation.setOptions({
+  modelSelection: 1,
+});
+
+selfieSegmentation.onResults(onResults);
 
 const onewebMeet = async () => {
-  await createOWTStream()
-  screateMPStream()
-
-  console.log("---- stream ----")
-  console.log(stream)
-  console.log("---- processedstream ----")
-  console.log(processedstream)
-
-  publishStream()
-}
+  // await createOWTStream()
+  initConference();
+  initStream();
+  await camera.start();
+  getProcessedStream();
+  publishStream();
+};
--- html/js/ui.js
+++ html/js/ui.js
@@ -1,116 +1,116 @@
 $(document).ready(function () {
-  $("#container").css("display", "none");
-  $("#login").fadeIn();
-});
+  $("#container").css("display", "none")
+  $("#login").fadeIn()
+})
 
 function loginDisableVideo() {
-  $("#login-resolution").slideUp();
+  $("#login-resolution").slideUp()
 }
 
 function loginEnableVideo() {
-  $("#login-resolution").slideDown();
+  $("#login-resolution").slideDown()
 }
 
 function showMain() {
-  $("#login").hide(200);
-  $("#wrapper").fadeIn(400);
+  $("#login").hide(200)
+  $("#wrapper").fadeIn(400)
 }
 async function login() {
-  showMain();
-  localName = $("#join").val();
+  showMain()
+  localName = $("#join").val()
   if (localName !== "") {
-    $("#navuser").html("Logged in: " + localName);
-    onewebMeet();
+    $("#navuser").html("Logged in: " + localName)
+    onewebMeet()
   }
 }
 
 $(document).ready(function () {
   $(".buttonset>a").click(function () {
-    console.log($(this));
-    $(this).siblings("a").removeClass("selected");
-    $(this).addClass("selected");
-  });
+    console.log($(this))
+    $(this).siblings("a").removeClass("selected")
+    $(this).addClass("selected")
+  })
 
   // name
   if (window.location.search.slice(0, 6) === "?room=") {
-    roomId = window.location.search.slice(6);
+    roomId = window.location.search.slice(6)
   }
 
   if (window.location.protocol === "https:") {
   } else {
-    // $('#screen-btn').addClass('disabled');
+    // $('#screen-btn').addClass('disabled')
   }
 
   $(document).on("click", "#pauseVideo", function () {
-    toggleVideo();
-  });
+    toggleVideo()
+  })
 
   $(document).on("click", "#pauseAudio", function () {
-    toggleAudio();
-  });
+    toggleAudio()
+  })
 
   $(document).on("click", ".mute", function () {
     // unmute
-    var id = parseInt($(this).parent().parent().attr("id").slice(7));
-    toggleMute(id, false);
-    $(this).addClass("unmute").removeClass("mute");
-  });
+    var id = parseInt($(this).parent().parent().attr("id").slice(7))
+    toggleMute(id, false)
+    $(this).addClass("unmute").removeClass("mute")
+  })
 
   $(document).on("click", ".unmute", function () {
     // mute
-    var id = parseInt($(this).parent().parent().attr("id").slice(7));
-    toggleMute(id, true);
-    $(this).addClass("mute").removeClass("unmute");
-  });
+    var id = parseInt($(this).parent().parent().attr("id").slice(7))
+    toggleMute(id, true)
+    $(this).addClass("mute").removeClass("unmute")
+  })
 
   //TODO:mute others
   $(document).on("dblclick", ".muteShow", function () {
     // mute others
-    var muteId = $(this).siblings(".userID").text();
+    var muteId = $(this).siblings(".userID").text()
     var msg = {
       type: "force",
-    };
+    }
     forwardStreamMap.forEach((stream, key) => {
       if (stream.info.owner === muteId && stream.media.audio) {
         if (stream.media.audio.status === "active") {
           pauseStream(roomId, stream.id, "audio", () =>
             chgMutePic(muteId, true)
-          );
+          )
         } else {
           playStream(roomId, stream.id, "audio", () =>
             chgMutePic(muteId, false)
-          );
+          )
         }
       }
-    });
-  });
+    })
+  })
 
   $("#text-send").keypress(function (event) {
     if ($(this)[0].scrollHeight > $(this)[0].clientHeight) {
-      $(this).height($(this)[0].scrollHeight);
-      $("#text-content").css("bottom", $(this)[0].scrollHeight + "px");
+      $(this).height($(this)[0].scrollHeight)
+      $("#text-content").css("bottom", $(this)[0].scrollHeight + "px")
     }
     if (event.keyCode === 13) {
-      event.preventDefault();
+      event.preventDefault()
       // send msg when press enter
-      sendIm();
+      sendIm()
     }
-  });
+  })
 
-  $(window).resize(function () {});
+  $(window).resize(function () {})
 
-  checkMobile();
+  checkMobile()
 
   setInterval(() => {
-    const d = new Date();
-    $("#clock").html(getTime() + ":");
-    $("#mill").html(d.getMilliseconds());
-  }, 1);
-});
+    const d = new Date()
+    $("#clock").html(getTime() + ":")
+    $("#mill").html(d.getMilliseconds())
+  }, 1)
+})
 
 $(window).on("unload", function () {
-  userExit();
-});
+  userExit()
+})
 
 function checkMobile() {
   if (
@@ -118,92 +118,92 @@ function checkMobile() {
       navigator.userAgent.toLowerCase()
     )
   ) {
-    isMobile = true;
-    $("#im-btn").hide();
+    isMobile = true
+    $("#im-btn").hide()
   }
 }
 
 const getTime = () => {
-  const time = new Date();
-  let hour = time.getHours();
-  hour = hour > 9 ? hour.toString() : "0" + hour.toString();
-  let mini = time.getMinutes();
-  mini = mini > 9 ? mini.toString() : "0" + mini.toString();
-  let sec = time.getSeconds();
-  sec = sec > 9 ? sec.toString() : "0" + sec.toString();
-  return hour + ":" + mini + ":" + sec;
-};
+  const time = new Date()
+  let hour = time.getHours()
+  hour = hour > 9 ? hour.toString() : "0" + hour.toString()
+  let mini = time.getMinutes()
+  mini = mini > 9 ? mini.toString() : "0" + mini.toString()
+  let sec = time.getSeconds()
+  sec = sec > 9 ? sec.toString() : "0" + sec.toString()
+  return hour + ":" + mini + ":" + sec
+}
 
 const toggleVideoUI = () => {
-  $("#tvn").toggleClass("block");
-  $("#tvb").toggleClass("block");
-  toggleVideo();
-};
+  $("#tvn").toggleClass("block")
+  $("#tvb").toggleClass("block")
+  toggleVideo()
+}
 
 const toggleAudioUI = () => {
-  $("#tan").toggleClass("block");
-  $("#tab").toggleClass("block");
-  toggleAudio();
-};
+  $("#tan").toggleClass("block")
+  $("#tab").toggleClass("block")
+  toggleAudio()
+}
 
 const toggleBRUI = () => {
-  $("#broff").toggleClass("block");
-  $("#bron").toggleClass("block");
-  $("#tbr").toggleClass("act");
+  $("#broff").toggleClass("block")
+  $("#bron").toggleClass("block")
+  $("#tbr").toggleClass("act")
 
   if( $("#broff").css("display") == "block" ) {
-    $("#bgscontainer").fadeIn();
+    $("#bgscontainer").fadeIn()
   } else {
-    $("#bgscontainer").fadeOut();
+    $("#bgscontainer").fadeOut()
   }
-  // toggleVideo();
-};
+  isbr = !isbr
+}
 
 const checkLeftBar = () => {
   if (
     $("#participants").css("display") == "block" ||
     $("#conversation").css("display") == "block"
   ) {
-    $("#left-bar").fadeIn();
-    $("footer").addClass("moveleft");
+    $("#left-bar").fadeIn()
+    $("footer").addClass("moveleft")
   }
 
   if (
     $("#participants").css("display") !== "block" &&
     $("#conversation").css("display") !== "block"
   ) {
-    $("#left-bar").fadeOut();
-    $("footer").removeClass("moveleft");
+    $("#left-bar").fadeOut()
+    $("footer").removeClass("moveleft")
   }
 
   if (
     $("#participants").css("display") == "block" &&
     $("#conversation").css("display") !== "block"
   ) {
-    $("#user-list").addClass("full");
-    $("#text-content").removeClass("full");
+    $("#user-list").addClass("full")
+    $("#text-content").removeClass("full")
   } else if (
     $("#participants").css("display") !== "block" &&
     $("#conversation").css("display") == "block"
   ) {
-    $("#user-list").removeClass("full");
-    $("#text-content").addClass("full");
+    $("#user-list").removeClass("full")
+    $("#text-content").addClass("full")
   } else {
-    $("#user-list").removeClass("full");
-    $("#text-content").removeClass("full");
+    $("#user-list").removeClass("full")
+    $("#text-content").removeClass("full")
   }
-};
+}
 
 const toggleUserList = () => {
-  $("#participants").toggle();
-  checkLeftBar();
-  $("#tuloff").toggleClass("block");
-  $("#tulon").toggleClass("block");
-};
+  $("#participants").toggle()
+  checkLeftBar()
+  $("#tuloff").toggleClass("block")
+  $("#tulon").toggleClass("block")
+}
 
 const toggleMessage = () => {
-  $("#conversation").toggle();
-  checkLeftBar();
-  $("#tmoff").toggleClass("block");
-  $("#tmon").toggleClass("block");
-};
+  $("#conversation").toggle()
+  checkLeftBar()
+  $("#tmoff").toggleClass("block")
+  $("#tmon").toggleClass("block")
+}
