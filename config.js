25ca332f72bc79806a889a0131ace3af7e955a20
Belem Zhang <belem.zhang@intel.com>
2022-08-19 13:43:33 +0800

    owt ip update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 module.exports = {
   id: '5df9ca6f7415937c7a91d774',
   key: 'rGtTQokQM/OeG/9oDzK9TtFjd+OOeUmFN2dZl52mvaI4cSj1waduIJB8x21Wa9MaGqtZzV1KTWBvr7heBIgSjQjQyeBWI0RFzCTSyhFtd9jmZ994xE50Gkmb2zxkQYALef8oj8do3gT/cWfOfgq1zPooCkRtbMK1xm44Avduyj4=',
-  url: 'https://10.239.115.78:3000/',
+  url: 'https://10.239.115.53:3000/',
   httpPort: 8008,
   httpsPort: 8080,
 };
